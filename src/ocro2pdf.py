974483d41c2e22de549464b98416fb3d7160b12b
joost <joost@port-3436.kl.dfki.de>
2010-03-23 14:26:40 +0100

    Reverted the useless change

--- src/ocro2pdf.py
+++ src/ocro2pdf.py
@@ -6,7 +6,7 @@
 # 
 # You may not use this file except under the terms of the accompanying license.
 # 
-# cLicensed under the Apache License, Version 2.0 (the "License"); you
+# Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License. You may
 # obtain a copy of the License at http:#www.apache.org/licenses/LICENSE-2.0
 # 
