8d26511af04fcb39be07c2b6e85fa3bcaaf4f976
joost <joost@port-3436.kl.dfki.de>
2010-05-05 10:02:21 +0200

    Modified and commented Oulu test to allow testing on smaller subsets

--- src/test-oulu-decapod-genpdf.py
+++ src/test-oulu-decapod-genpdf.py
@@ -38,7 +38,7 @@ import shlex, subprocess
 # global variables:
 verbose     = 0         # output debugging information
 dpi         = 200       # resolution of the ouput images in the pdf
-tempLogDir  = "testOutput/"
+tempLogDir  = "."       # directory where the output log will be saved
 
 # ========= ImagePDF =========
 
@@ -50,14 +50,17 @@ def main(sysargv):
     # make global variables available
     global verbose 
     global dpi
-    pageWidth  = 21.0; # default page width, DIN A4
-    pageHeight = 29.7; # default page height, DIN A4
+    pageWidth  = 21.0; # default page width in cm, DIN A4
+    pageHeight = 29.7; # default page height in cm, DIN A4
+    outDir="."
+    N = -1             # number of samples to use for the test
+    testSetDir=""      # path to the oulu-doc/data/ directory
     # parse command line options
     if len(sysargv) == 1:
         usage(sysargv[0])
         sys.exit(0)    
     try:
-        optlist, args = getopt.getopt(sysargv[1:], 'hd:l:', ['help','dir=','log='])
+        optlist, args = getopt.getopt(sysargv[1:], 'hd:l:o:n:', ['help','dir=','log=','out=','num='])
         #print(optlist, args)
     except getopt.error, msg:
         print msg
@@ -76,48 +79,70 @@ def main(sysargv):
             verbose = int(a)
         if o in ("-r", "--resolution"):
             dpi = int(a)
+        if o in ("-o", "--out"):
+            outDir = a
+        if o in ("-n", "--num"):
+            N = int(a)
 
-    testSet = readTestSet(testSetDir)
+    # read oulu data dir and extract the full paths of the high res. files
+    allTestSet = readTestSet(testSetDir)
 
-    #print "============="
-    #print testSet
-    #print "============="
+    # generate a test set containing all (N<0) or N samples
+    testSet=[]
+    print N
+    if (N<0):
+        testSet = allTestSet
+    else:
+        step = len(allTestSet)/N
+        for i in range(N):
+            print allTestSet[i*step]
+            testSet.append(allTestSet[i*step])
 
-    smallSet = testSet[1:3]
+    if (verbose>1):
+        print "====== Test Set ======="
+        print testSet
+        print "============="
 
-    errors, total = runTest(testSet)
+    errors, total = runTest(testSet,outDir)
     
     if(len(errors)==0):
         print("===== SUCCESS! ====")
         print("  all %d tests went through!" %(total))
     else:
+        print("===== ERRORS OCCURED! ====")
+        print("  %d of %d tests went through!" %(total-len(errors),total))
         print("===== ERROR REPORT ====")
         print("exit code\tPDF type\timage\ttempDir")
         for i in errors:
             print("%d       \t%d      \t%s\t%s" %(i[0],i[1],i[2],i[3]))
     
     
-    
             
-        
-        
+## 
+#  Parse the oulu directory structure and generate a list of file names
+#  @param:  path to the data directory of the oulu data set
+#  @return: list of filenames (including absolute path)
+##
 def readTestSet(testSetDir):
     fileNames = []
     print testSetDir
     for root, dirs, files in os.walk(testSetDir):
-        #print "=== 0 ==="
-        #print root
-        #print "=== 1 ==="
-        #print dirs
-        #print "=== 2 ==="
-        #print files
         for name in files:
-            #print name, name[len(name)-3:len(name)]
             if (name.find("_") < 0 and name[len(name)-3:len(name)]=="jpg"):
                 fileNames.append(root+"/"+name)
     return fileNames
     
-    
+## 
+#  Method that starts a single PDF generation task. Used for parallelization
+#  @param:  fn: filename of the image (including full path)
+#  @param:  tempDir: path where the book directory will be generated
+#  @param:  tempLogDir: directory where the log will be saved
+#  @param:  t: type of the PDF to be generated
+#  @return: retcode: return code of the PDF generation
+#  @return: t: type of the PDF to be generated
+#  @param:  fn: filename of the image (including full path)
+#  @param:  tempDir: path where the book directory was generated
+##    
 def executeCmd(fn, tempDir, tempLogDir, t):
     global verbose
     bn    = fn[fn.rfind("/"):len(fn)]
@@ -126,19 +151,21 @@ def executeCmd(fn, tempDir, tempLogDir, t):
     # remove the generated DIR as decapod-genpdf will create it
     os.rmdir(tempDir)
     cmd_genpdf = "decapod-genpdf.py -b %s -d %s -p %s -t %s > %s" %(fn,tempDir,pdfFN,t,logFN)
-    #cmd_genpdf = "decapod-genpdf.py"
-    #args_genpdf = "-b %s -d %s -p %s -t %s " %(fn,tempDir,pdfFN,t,logFN)
     if verbose>=1:
         print(cmd_genpdf)
-    #pID = os.popen(cmd_genpdf)
-    #print "===== pID ====="
-    #print pID
     args = shlex.split(cmd_genpdf)
     retcode = subprocess.call(args)
     return retcode,t,fn,tempDir
 
 
-def runTest(testSet):
+## 
+#  Run the test on the test set
+#  @param:  testSet: list of image files
+#  @param:  outDir: directory where to put the book dirs, PDFs, etc.
+#  @return: errors: error information
+#  @return: totalTests: how many images were (successfully or not) converted to PDF
+## 
+def runTest(testSet,outDir):
     global tempLogDir
     q = []
     processes=Pool()
@@ -150,20 +177,17 @@ def runTest(testSet):
         for test in testSet:
             totalTests += 1
             print "    === Image %s ===" %(test)
-            tempDir=tempfile.mkdtemp()+"/";
+            bn    = test[test.rfind("/")+1:len(test)-4]
+            tempDir=tempfile.mkdtemp(prefix="book-%d-%s-" %(t,bn),dir=outDir)+"/";
             q.append(processes.apply_async(executeCmd,(test, tempDir, tempLogDir, t)))
     
   
     processes.close()
     processes.join()
-    
-    #results=zeros([4],int) # TOT, HPS corr., VPS corr.
     errors=[]
-    #print(q)
     for i in q:
         print(i)
         data=i.get()
-        #print data
         if(data[0]!=0):
             errors.append(data)
     return errors, totalTests
@@ -175,7 +199,9 @@ def usage(progName):
     print "\n%s [OPTIONS] -dir ocroDir -pdf out.pdf\n\n"\
           "   -d, --dir          directory of the Oulu dataset\n"\
           "   -l, --log          log file containing the report\n"\
+          "   -o, --out          directory where the temporary files and PDFs will be placed\n"\
           " Options:\n"\
+          "   -n, --num:         number of test documents to use\n"\
           "   -h, --help:        print this help\n"\
           "   -v, --verbose:     verbose level [0,1,2,3]\n"
 
